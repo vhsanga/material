66a06a5a1a66991e19b1a2c83938070bbb02b791
victor_sanga <victorsanga@coopdaquilema.com>
2019-05-02 16:04:31 -0500

    validaciones campos

--- public/app-js/home/init.js
+++ public/app-js/home/init.js
@@ -3,9 +3,10 @@ $(function(){
 
 
 	$("#callAjaxError").click(function (event){
-		UTIL.executarGet("/listarProducto","Se han obtenido la lista de productos",function (dataRes) {
+		/*UTIL.executarGet("/listarProducto","Se han obtenido la lista de productos",function (dataRes) {
 			 console.log('dataRes', dataRes);
-		});
+		});*/
+		UTIL.formAJson('formEjemplo');
 	});
 	$("#callAjaxSuccess").click(function (event){
 		UTIL.executarPost("/crearProducto",{},"Se ha creado el producto",function (dataRes) {
--- public/app-js/util.js
+++ public/app-js/util.js
@@ -11,7 +11,7 @@
  }
 
 
- UTIL.prototype.mensaje = function(texto, tipo) {
+ UTIL.prototype.mensaje = function(texto, tipo, maximo=1) {
  	texto = "<p style='color: white; font-size: 14px;'>"+texto+"</p>"
 	//swal(this.ESPERE);
 	var head='';
@@ -42,30 +42,30 @@
 		head='<b>!Ups! hay un problema </b>'
 	}
 	$.toast({
-			heading: head,
-			text: texto,
-			position: 'top-right',
-			loaderBg:_loaderbg,
-			bgColor: _bgcolor,
-			icon: _icon,
-			hideAfter: _hideAfter, 
-			showHideTransition: 'plain',
-			stack: 1,
-			loader:_loader
-		});
+		heading: head,
+		text: texto,
+		position: 'top-right',
+		loaderBg:_loaderbg,
+		bgColor: _bgcolor,
+		icon: _icon,
+		hideAfter: _hideAfter, 
+		showHideTransition: 'plain',
+		stack: maximo,
+		loader:_loader
+	});
 };
 
 UTIL.prototype.executarPost = function(url, dataSend, msjExito, callback){
 	UTIL.mensaje(UTIL.ESPERE, UTIL.MENSAJE_TIPO_PROCCESS);
 	$.post( url,dataSend).done(function( dataRes ) {
 		UTIL.mensaje(msjExito, UTIL.MENSAJE_TIPO_SUCCESS);
-        callback(dataRes);
-    }).fail(function(e) {
-      try{
-      	   console.error(e);
-           UTIL.mensaje(e.statusText,UTIL.MENSAJE_TIPO_ERROR);
-       }catch(e){  UTIL.mensaje(this.ERROR_AJAX_RESULT,UTIL.MENSAJE_TIPO_ERROR) }
-    });
+		callback(dataRes);
+	}).fail(function(e) {
+		try{
+			console.error(e);
+			UTIL.mensaje(e.statusText,UTIL.MENSAJE_TIPO_ERROR);
+		}catch(e){  UTIL.mensaje(this.ERROR_AJAX_RESULT,UTIL.MENSAJE_TIPO_ERROR) }
+	});
 };
 
 
@@ -73,18 +73,81 @@ UTIL.prototype.executarGet = function(url,  msjExito, callback){
 	UTIL.mensaje(UTIL.ESPERE, UTIL.MENSAJE_TIPO_PROCCESS);
 	$.get(url).done(function( dataRes ) {
 		UTIL.mensaje(msjExito, UTIL.MENSAJE_TIPO_SUCCESS);
-        callback(dataRes);
-    }).fail(function(e) {
-      try{
-      	 console.error(e);
-         UTIL.mensaje(e.statusText,UTIL.MENSAJE_TIPO_ERROR);
-       }catch(e){  UTIL.mensaje(this.ERROR_AJAX_RESULT,UTIL.MENSAJE_TIPO_ERROR) }
-    });
+		callback(dataRes);
+	}).fail(function(e) {
+		try{
+			console.error(e);
+			UTIL.mensaje(e.statusText,UTIL.MENSAJE_TIPO_ERROR);
+		}catch(e){  UTIL.mensaje(this.ERROR_AJAX_RESULT,UTIL.MENSAJE_TIPO_ERROR) }
+	});
+};
+
+
+UTIL.prototype.formAJson = function(idFormHtml, validar=true){ 
+	/* idFormHtml = id del formulario que desamos deserialziar
+	*  todos los componentes que se desa deserializar debe tener un 'name' que sera el KEY del json
+	*  todos los componente a deseralizar deben ser de la clase css '_formJSON'  
+	*  en caso de 'validar' =true, se debe hacer el proceso de validacion de campos
+	*  'required'  es el tag que sirve para definir si el campo es obligatorio
+	*  'requiredMessage' es el mensaje de alerta en caso de ser campo obligatorio
+	*/
+	var items= document.getElementById(idFormHtml).getElementsByClassName('_formJSON');
+
+	if(validar){
+		var mensaje=''
+		for(var i=0; i<items.length; i++){
+			items[i].classList.remove('form-control-warning')
+			items[i].parentNode.classList.remove('has-warning')
+			if(items[i].getAttribute('required')==='true'){
+				if(items[i].value===''){					
+					mensaje=items[i].getAttribute('requiredMessage')==null ? 'El campo "'+items[i].name+'" es obligatorio': items[i].getAttribute('requiredMessage')
+					UTIL.mensaje(mensaje, UTIL.MENSAJE_TIPO_WARNING,4);
+					items[i].classList.add('form-control-warning')
+					items[i].parentNode.classList.add('has-warning')
+					console.log(items[i].parentNode);
+				}
+			}
+			
+			
+		}
+	}
+
+
+
+	
+	var json={};
+	var valorRadio='';
+	for(var i=0; i<items.length; i++){
+		if(items[i].nodeName==='INPUT'  || items[i].nodeName==='SELECT'  || items[i].nodeName==='TEXTAREA'){
+			if(items[i].getAttribute('type')==='checkbox'){
+				json[items[i].name]=[];
+			}else{
+				json[items[i].name]='';	
+			}
+		}
+	}
+	for(var i=0; i<items.length; i++){
+		if(items[i].nodeName==='INPUT'  || items[i].nodeName==='SELECT' || items[i].nodeName==='TEXTAREA'){
+			if(items[i].getAttribute('type')==='radio'){
+				if(items[i].checked){
+					json[items[i].name]=items[i].value;
+				}
+			}else if(items[i].getAttribute('type')==='checkbox'){
+				if(items[i].checked){
+					json[items[i].name].push(items[i].value);
+				}
+			}else {
+				json[items[i].name]=items[i].value
+			}
+		}
+	}
+	console.log(json);
+	return json;
 };
 
 
 
 
 
- 
+
 var UTIL = new UTIL();
